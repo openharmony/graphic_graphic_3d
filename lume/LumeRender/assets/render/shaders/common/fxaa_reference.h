7d5c1125e07d939c332ab7df9d1f87b44c1dc1ba
xxx <xxx>
2022-06-29 17:39:13 +0800

    3d fxaa default
    Match-id-e1d473f8d5280b3481d8b1ed0fcfa51238457e52
    
    Signed-off-by: doggy-zhang <zhangwenqi9@huawei.com>

---
+++ lume/LumeRender/assets/render/shaders/common/fxaa_reference.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
+ */
+
+#ifndef SHADERS__COMMON__FXAA_REFERENCE_H
+#define SHADERS__COMMON__FXAA_REFERENCE_H
+
+vec4 FxaaPixelShader(
+    vec2 pos,
+    vec4 fxaaConsolePosPos,
+    texture2D tex,
+    sampler samp,
+    vec2 fxaaQualityRcpFrame,
+    vec4 fxaaConsoleRcpFrameOpt,
+    vec4 fxaaConsoleRcpFrameOpt2,
+    vec4 fxaaConsole360RcpFrameOpt2,
+    float fxaaQualitySubpix,
+    float fxaaQualityEdgeThreshold,
+    float fxaaQualityEdgeThresholdMin,
+    float fxaaConsoleEdgeSharpness,
+    float fxaaConsoleEdgeThreshold,
+    float fxaaConsoleEdgeThresholdMin,
+    vec4 fxaaConsole360ConstDir
+) {
+    return vec4();
+}
+#endif // SHADERS__COMMON__FXAA_REFERENCE_H
+
