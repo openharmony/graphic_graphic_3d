d744187649cc0e79656d449cc4cf43e8d7c2d62b
Mason <liuxiaolong61@huawei.com>
2025-03-10 16:29:22 +0800

    code format 2
    Signed-off-by: Mason <liuxiaolong61@huawei.com>

--- kits/js/include/MaterialPropertyJS.h
+++ kits/js/include/MaterialPropertyJS.h
@@ -12,8 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef _MATERIAL_PROPERTY_JS_H_
-#define _MATERIAL_PROPERTY_JS_H_
+#ifndef MATERIAL_PROPERTY_JS_H
+#define MATERIAL_PROPERTY_JS_H
 
 #include <meta/interface/intf_object.h>
 
