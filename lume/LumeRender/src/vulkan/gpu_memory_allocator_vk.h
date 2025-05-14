7e5048e234fe6dd71ce6991ff8b4521933b888c4
h00868521 <huzhe10@huawei.com>
2025-05-14 06:57:11 +0000

    suit m133 graphic_3d
    Signed-off-by: h00868521 <huzhe10@huawei.com>
    Change-Id: Ida5ab60fffa9cbbbd16af742c34022da08cb2f31

--- lume/LumeRender/src/vulkan/gpu_memory_allocator_vk.h
+++ lume/LumeRender/src/vulkan/gpu_memory_allocator_vk.h
@@ -20,7 +20,11 @@
 // clang-format off
 #include <vulkan/vulkan_core.h>
 #ifdef __OHOS__
+#ifdef USE_M133_SKIA
+#include <third_party/externals/vulkanmemoryallocator/include/vk_mem_alloc.h>
+#else
 #include <third_party/vulkanmemoryallocator/include/vk_mem_alloc.h>
+#endif
 #else
 #include <VulkanMemoryAllocator/src/vk_mem_alloc.h>
 #endif
