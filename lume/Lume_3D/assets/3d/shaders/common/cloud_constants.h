903206d1d6cce24dbc4b9649e6b5355f201ebc19
lwx1279465 <liuguanpeng1@huawei.com>
2025-06-10 16:58:53 +0800

    fix 3d csec
    Signed-off-by: lwx1279465 <liuguanpeng1@huawei.com>

--- lume/Lume_3D/assets/3d/shaders/common/cloud_constants.h
+++ lume/Lume_3D/assets/3d/shaders/common/cloud_constants.h
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef CLOUDS_CONSTANTS_H
 #define CLOUDS_CONSTANTS_H
 
@@ -20,10 +34,10 @@ const float weatherMapScale = 8.0;
 const float LIGHTING_SAMPLES_1TO5_STEP_SIZE = 60.0;
 const float LIGHTING_SAMPLES_6_STEP_SIZE = 800.0;
 
-// status = 0 – 2,000 meters
-// cumulus = 200 – 2,000 meters
-// Cumulonimbus = 500–16,000 meters
-// Alto = 2,000–7,000 meters
+// status = 0  2,000 meters
+// cumulus = 200  2,000 meters
+// Cumulonimbus = 50016,000 meters
+// Alto = 2,0007,000 meters
 // Cirrus = 4,000-20,000 meters
 
 // first, lets define some constants to use (planet radius, position, and scattering coefficients)
