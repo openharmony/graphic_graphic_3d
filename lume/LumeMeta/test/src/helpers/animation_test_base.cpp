9d4cc6348bb437f6f26b30eca512765367786e32
doggy-Zhang <zhangwenqi9@huawei.com>
2025-07-29 08:41:25 +0000

    add helpers
    Signed-off-by: doggy-Zhang <zhangwenqi9@huawei.com>

---
+++ lume/LumeMeta/test/src/helpers/animation_test_base.cpp
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "animation_test_base.h"
+
+#include <gmock/gmock-matchers.h>
+
+#include <meta/interface/builtin_objects.h>
+#include <meta/interface/intf_object_registry.h>
+
+META_BEGIN_NAMESPACE()
+
+void AnimationTestBase::SetUp()
+{
+    ::testing::Test::SetUp();
+    clock_ = GetObjectRegistry().Create<IManualClock>(META_NS::ClassId::ManualClock);
+}
+
+TimeSpan AnimationTestBase::Time() const
+{
+    return clock_->GetTime();
+}
+
+void AnimationTestBase::IncrementClockTime(const TimeSpan& time)
+{
+    clock_->IncrementTime(time);
+}
+
+void AnimationTestBase::Update(const TimeSpan& time)
+{
+    IncrementClockTime(time);
+    GetAnimationController()->Step(clock_);
+}
+
+IClock::Ptr AnimationTestBase::GetTestClock() const
+{
+    return clock_;
+}
+
+IAnimationController::Ptr AnimationTestBase::GetAnimationController()
+{
+    return META_NS::GetAnimationController();
+}
+
+void AnimationTestBase::RunFrames(uint32_t frames, std::function<void(uint32_t frame)> updateFn)
+{
+    // Sleep for a bit before first update to allow e.g. animations to have some time
+    // to proceed before the first animation frame.
+    IncrementClockTime(TimeSpan::Milliseconds(10)); // 10: param
+    for (uint32_t i = 0; i < frames; i++) {
+        GetAnimationController()->Step(GetTestClock());
+        updateFn(i + 1);
+        if (i < frames - 1)
+            IncrementClockTime(TimeSpan::Milliseconds(10)); // 10: param
+    }
+}
+
+void AnimationTestBase::StepAnimationController(int64_t stepMs)
+{
+    clock_->IncrementTime(TimeSpan::Milliseconds(stepMs));
+    GetAnimationController()->Step(GetTestClock());
+}
+
+void AnimationTestBase::StepAnimations(
+    const std::vector<IAnimation::Ptr> animations, uint32_t frames, std::function<void(uint32_t frame)> updateFn)
+{
+    StepAnimations(animations, frames, 10, updateFn); // 10: param
+}
+
+void AnimationTestBase::StepAnimations(const std::vector<IAnimation::Ptr> animations, uint32_t frames,
+    int64_t frameStepMs, std::function<void(uint32_t frame)> updateFn)
+{
+    IncrementClockTime(TimeSpan::Milliseconds(frameStepMs));
+    for (uint32_t i = 0; i < frames; i++) {
+        for (auto& animation : animations) {
+            animation->Step(GetTestClock());
+        }
+        if (updateFn) {
+            updateFn(i + 1);
+        }
+        if (i < frames - 1)
+            IncrementClockTime(TimeSpan::Milliseconds(frameStepMs));
+    }
+}
+
+META_END_NAMESPACE()
---
+++ lume/LumeMeta/test/src/helpers/test_data_helpers.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef META_TEST_TEST_DATA_HELPERS_H
+#define META_TEST_TEST_DATA_HELPERS_H
+
+#include <tuple>
+#include <type_traits>
+
+#include <gtest/gtest.h>
+
+#include <base/containers/type_traits.h>
+#include <base/containers/vector.h>
+
+#include <meta/base/namespace.h>
+
+META_BEGIN_NAMESPACE()
+
+template<typename T>
+struct TType {
+    using Type = T;
+    BASE_NS::vector<std::function<Type()>> values;
+
+    template<typename... Args, typename = BASE_NS::enable_if_t<std::conjunction_v<std::is_convertible<Args, Type>...>>>
+    TType(Args... args) : values { [args] { return static_cast<T>(args); }... }
+    {}
+
+    TType(std::initializer_list<std::function<Type()>> values) : values { values } {}
+};
+
+template<typename... T>
+struct TestTypes {
+    using Types = ::testing::Types<T...>;
+    using Tuple = std::tuple<TType<T>...>;
+
+    TestTypes(TType<T>... v) : values { v... } {}
+
+    template<typename Type>
+    auto GetValue(size_t index) const
+    {
+        auto v = std::get<TType<Type>>(values);
+        CORE_ASSERT_MSG(index < v.values.size(), "invalid test values");
+        return v.values[index]();
+    }
+
+    Tuple values;
+};
+
+META_END_NAMESPACE()
+
+#endif // META_TEST_TEST_DATA_HELPERS_H
