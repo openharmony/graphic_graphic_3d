46e6562fd53d013171a160728e9457282046ca30
lwx1279465 <liuguanpeng1@huawei.com>
2025-08-26 18:19:09 +0800

    add kits ets
    Signed-off-by: lwx1279465 <liuguanpeng1@huawei.com>

---
+++ kits/ets/include/NodeETS.h
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NODE_ETS_H
+#define NODE_ETS_H
+
+#include <memory>
+
+#include <meta/interface/intf_object.h>
+#include <scene/ext/component_util.h>
+#include <scene/interface/component_util.h>
+#include <scene/interface/intf_node.h>
+
+#include "SceneResourceETS.h"
+#include "Vec3Proxy.h"
+#include "QuatProxy.h"
+
+namespace OHOS::Render3D {
+class NodeETS : public SceneResourceETS, public std::enable_shared_from_this<NodeETS> {
+    friend bool operator==(const SCENE_NS::INode::Ptr node, const std::shared_ptr<NodeETS> nodeETS);
+    friend class CameraETS;
+
+public:
+    static std::shared_ptr<NodeETS> FromNative(const SCENE_NS::INode::Ptr &node);
+    enum NodeType { NODE = 1, GEOMETRY = 2, CAMERA = 3, LIGHT = 4, TEXT = 5 };
+
+    NodeETS(const NodeType &type, const SCENE_NS::INode::Ptr &node);
+    NodeETS(const SCENE_NS::INode::Ptr &node);
+    virtual ~NodeETS();
+
+    META_NS::IObject::Ptr GetNativeObj() const override;
+
+    NodeType GetNodeType();
+
+    std::string GetNodeName();
+    void SetNodeName(const std::string &name);
+
+    std::shared_ptr<Vec3Proxy> GetPosition();
+    void SetPosition(const BASE_NS::Math::Vec3 &position);
+
+    std::shared_ptr<Vec3Proxy> GetScale();
+    void SetScale(const BASE_NS::Math::Vec3 &scale);
+
+    std::shared_ptr<QuatProxy> GetRotation();
+    void SetRotation(const BASE_NS::Math::Quat &rotation);
+
+    std::string GetPath();
+    std::shared_ptr<NodeETS> GetParent();
+
+    std::shared_ptr<NodeETS> GetNodeByPath(const std::string &path);
+    // napi_value GetComponent(NapiApi::FunctionContext<BASE_NS::string> &ctx);
+
+    std::shared_ptr<NodeETS> GetChildContainer();  // returns a container object.
+
+    bool GetVisible();
+    void SetVisible(const bool visible);
+
+    std::shared_ptr<NodeETS> GetLayerMask();
+
+    bool GetLayerMaskEnabled(const uint32_t bit);
+    void SetLayerMaskEnabled(const uint32_t bit, const bool enabled);
+
+    size_t GetCount();
+    std::shared_ptr<NodeETS> GetChild(const uint32_t index);
+
+    void ClearChildren();
+    void InsertChildAfter(const std::shared_ptr<NodeETS> &childNode, const std::shared_ptr<NodeETS> &siblindNode);
+    void AppendChild(const std::shared_ptr<NodeETS> &childNode);
+    void RemoveChild(const std::shared_ptr<NodeETS> &childNode);
+
+protected:
+    SCENE_NS::INode::Ptr GetInternalNode()
+    {
+        return node_.lock();
+    }
+
+private:
+    NodeType type_;
+    SCENE_NS::INode::WeakPtr node_{nullptr};
+    std::shared_ptr<Vec3Proxy> posProxy_{nullptr};
+    std::shared_ptr<Vec3Proxy> sclProxy_{nullptr};
+    std::shared_ptr<QuatProxy> rotProxy_{nullptr};
+};
+}  // namespace OHOS::Render3D
+#endif  // NODE_ETS_H
---
+++ kits/ets/src/PostProcessETS.cpp
@@ -0,0 +1,146 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "PostProcessETS.h"
+#include "Utils.h"
+
+namespace OHOS::Render3D {
+std::shared_ptr<PostProcessETS> PostProcessETS::FromJS(
+    const std::shared_ptr<TonemapETS> tonemap, const std::shared_ptr<BloomETS> bloom)
+{
+    auto pp = std::shared_ptr<PostProcessETS>(new PostProcessETS());
+    pp->tonemap_ = tonemap;
+    pp->bloom_ = bloom;
+    return pp;
+}
+
+PostProcessETS::PostProcessETS(const SCENE_NS::ICamera::Ptr camera, const SCENE_NS::IPostProcess::Ptr pp)
+    : camera_(camera), postProc_(pp)
+{}
+
+PostProcessETS::PostProcessETS()
+{}
+
+PostProcessETS::~PostProcessETS()
+{
+    tonemap_.reset();
+    bloom_.reset();
+    postProc_.reset();
+    camera_.reset();
+}
+
+std::shared_ptr<TonemapETS> PostProcessETS::GetToneMapping()
+{
+    if (!postProc_) {
+        // The configuration data from ETS is stored in tonemap_.
+        return tonemap_;
+    }
+    if (!tonemap_) {
+        SCENE_NS::ITonemap::Ptr toneValue = META_NS::GetValue(postProc_->Tonemap());
+        tonemap_ = std::make_shared<TonemapETS>(postProc_, toneValue);
+    }
+
+    if (tonemap_->IsEnabled()) {
+        return tonemap_;
+    } else {
+        // tonemap disabled.
+        return nullptr;
+    }
+}
+
+void PostProcessETS::SetToneMapping(const std::shared_ptr<TonemapETS> tonemap)
+{
+    if (!postProc_) {
+        return;
+    }
+
+    if (!tonemap_) {
+        SCENE_NS::ITonemap::Ptr toneValue = META_NS::GetValue(postProc_->Tonemap());
+        tonemap_ = std::make_shared<TonemapETS>(postProc_, toneValue);
+    }
+
+    if (tonemap) {
+        if (tonemap_->StrictEqual(tonemap)) {
+            // setting the exactly the same tonemap setting. do nothing.
+            return;
+        }
+        if (tonemap_->IsMatch(tonemap)) {
+            ExecSyncTask([this, tonemap]() {
+                tonemap_->SetEnabled(true);
+                tonemap_->SetType(tonemap->GetType());
+                tonemap_->SetExposure(tonemap->GetExposure());
+                return META_NS::IAny::Ptr{};
+            });
+        } else {
+            CORE_LOG_F("Invalid state. Can't change the post process of a tonemap if it already has one");
+        }
+    } else {
+        tonemap_->SetEnabled(false);
+    }
+}
+
+std::shared_ptr<BloomETS> PostProcessETS::GetBloom()
+{
+    if (!postProc_) {
+        // The configuration data from ETS is stored in bloom_.
+        return bloom_;
+    }
+    if (!bloom_) {
+        SCENE_NS::IBloom::Ptr bloomValue = META_NS::GetValue(postProc_->Bloom());
+        bloom_ = std::make_shared<BloomETS>(postProc_, bloomValue);
+    }
+
+    if (bloom_->IsEnabled()) {
+        return bloom_;
+    } else {
+        // bloom disabled.
+        return nullptr;
+    }
+}
+
+void PostProcessETS::SetBloom(const std::shared_ptr<BloomETS> bloom)
+{
+    if (!postProc_) {
+        return;
+    }
+    if (!bloom_) {
+        SCENE_NS::IBloom::Ptr bloomValue = META_NS::GetValue(postProc_->Bloom());
+        bloom_ = std::make_shared<BloomETS>(postProc_, bloomValue);
+    }
+    if (bloom) {
+        if (bloom_->StrictEqual(bloom)) {
+            // setting the exactly the same bloom setting. do nothing.
+            return;
+        }
+        if (bloom_->IsMatch(bloom)) {
+            ExecSyncTask([this, bloom]() {
+                bloom_->SetEnabled(true);
+                bloom_->SetType(bloom->GetType());
+                bloom_->SetQuality(bloom->GetQuality());
+                bloom_->SetThresholdHard(bloom->GetThresholdHard());
+                bloom_->SetThresholdSoft(bloom->GetThresholdSoft());
+                bloom_->SetScatter(bloom->GetScatter());
+                bloom_->SetScaleFactor(bloom->GetScaleFactor());
+                bloom_->SetAmountCoefficient(bloom->GetAmountCoefficient());
+                return META_NS::IAny::Ptr{};
+            });
+        } else {
+            CORE_LOG_F("Invalid state. Can't change the post process of a bloom if it already has one");
+        }
+    } else {
+        bloom_->SetEnabled(false);
+    }
+}
+}  // namespace OHOS::Render3D
